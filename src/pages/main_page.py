52f48acb209a31142d2ad816d0dc2d092a5c3acd
LayalAlsowayigh <Alsoli01@pfw.edu>
2026-02-26 18:40:00 -0500

    Update main page navigation

--- src/pages/main_page.py
+++ src/pages/main_page.py
@@ -1,6 +1,6 @@
 import customtkinter as ctk
 from pages.schedule_page import SchedulePage
-
+from pages.assignments_page import AssignmentsPage
 
 class MainPage(ctk.CTkFrame):
     def __init__(self, parent, app):
@@ -31,13 +31,24 @@ class MainPage(ctk.CTkFrame):
         self.content.grid_columnconfigure(0, weight=1)
 
         # Internal pages inside main
+        btn_assignments = ctk.CTkButton(sidebar, text="Assignments", command=self._show_assignments)
+        btn_assignments.grid(row=2, column=0, padx=16, pady=(8, 8), sticky="ew")
         self.schedule_page = SchedulePage(self.content)
         self.schedule_page.grid(row=0, column=0, sticky="nsew")
 
-        self._show_schedule()
+        self.assignments_page = AssignmentsPage(self.content, app)
+        self.assignments_page.grid(row=0, column=0, sticky="nsew")
 
+        self._show_schedule()
+        
     def _show_schedule(self):
         self.schedule_page.tkraise()
+        
 
     def _logout(self):
         self.app.show_page("LoginPage")
+
+    def _show_assignments(self):
+        self.assignments_page.refresh()
+        self.assignments_page.tkraise()
+       git remote set-url origin https://github.com/alpic29/StudentPlanner.git
