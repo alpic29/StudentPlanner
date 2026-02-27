3f7e0e9b7d0884cfb6c927b80cdb284b64169231
Layal ALsowayigh <Alsoli01@pfw.edu>
2026-02-26 19:12:04 -0500

    Add load_tasks and save_tasks functions

--- src/backend/storage.py
+++ src/backend/storage.py
@@ -1,1 +1,14 @@
+import json
+import os
 
+DATA_FILE = os.path.join(os.path.dirname(__file__), "..", "data", "tasks.json")
+
+def load_tasks():
+    if not os.path.exists(DATA_FILE):
+        return []
+    with open(DATA_FILE, "r") as file:
+        return json.load(file)
+
+def save_tasks(tasks):
+    with open(DATA_FILE, "w") as file:
+        json.dump(tasks, file, indent=4)
